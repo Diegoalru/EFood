20cd42aa923feed4fc34271f9116d0bad912e3d6
Diego Rubí Salas <diegoalru@gmail.com>
2020-06-26 10:50:13 -0600

    Nuevos modelos.

---
+++ EFood_Service/EFoodBLL/ClientModels/ShoppingCart.cs
@@ -0,0 +1,9 @@
+namespace EFoodBLL.ClientModels
+{
+    public class ShoppingCart
+    {
+        public string Transaction { get; set; }
+        public int ProductPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
