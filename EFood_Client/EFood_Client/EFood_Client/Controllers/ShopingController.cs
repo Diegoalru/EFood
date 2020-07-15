b8956e05e60c55597ed1d9aa06178e60531d31bc
Diego Rubí Salas <diegoalru@gmail.com>
2020-07-15 01:18:14 -0600

    Creada las vistas de la app cliente.

---
+++ EFood_Client/EFood_Client/EFood_Client/Controllers/ShopingController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace EFood_Client.Controllers
+{
+    public class ShopingController : Controller
+    {
+        public ActionResult LineProducts()
+        {
+            return View();
+        }
+    }
+}
