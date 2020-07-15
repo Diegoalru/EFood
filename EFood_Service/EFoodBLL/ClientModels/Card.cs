10f04b3c9e76b3f012db8bd9866255339f9ec8cb
Diego Rubí Salas <diegoalru@gmail.com>
2020-07-15 01:03:03 -0600

    Cambios minimos en el modelo Card.cs.

--- EFood_Service/EFoodBLL/ClientModels/Card.cs
+++ EFood_Service/EFoodBLL/ClientModels/Card.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace EFoodBLL.ClientModels
 {
+    /// <summary>
+    /// Esta clase puede ser utilizada a la hora de mostrar la lista de tarjetas que existen en la base de datos.
+    /// </summary>
     public class CardList
     {
         public int PkCode { get; set; }
+        
+        [Required(ErrorMessage = "Ingrese el nombre del tipo de tarjeta.")]
+        [DisplayName("Tipo")]
+        [MaxLength(30, ErrorMessage = "No puede contener más de 30 carácteres.")]
         public string Type { get; set; }
     }
-    
+
     public class Card_Client
     {
         public string CardOwner { get; set; }
