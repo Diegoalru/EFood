0aca8f6523f8a6b584b9730bc742943ff9011379
Diego Rubí Salas <diegoalru@gmail.com>
2020-07-15 01:25:25 -0600

    Fix conflicto con la clase ReturnPriceType.

--- EFood_Service/EFoodBLL/IntranetModels/PriceType.cs
+++ EFood_Service/EFoodBLL/IntranetModels/PriceType.cs
@@ -11,13 +11,6 @@ namespace EFoodBLL.IntranetModels
         public string Type { get; set; }
     }
 
-    public class ReturnPriceType
-    {
-        public int PkCode { get; set; }
-        public string Code { get; set; }
-        public string Type { get; set; }
-    }
-
     public class PriceTypeList
     {
         public int PkCode { get; set; }
