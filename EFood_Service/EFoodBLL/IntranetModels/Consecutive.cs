93fe76d5e319bc54be5f81a8b30c1e6eb0727cae
Marco Vinicio Najera Arguedas <mnajeraa298@ulacit.ed.cr>
2020-07-17 05:02:48 -0600

    Cambio formato en consecutivo y tipos de precio

--- EFood_Service/EFoodBLL/IntranetModels/Consecutive.cs
+++ EFood_Service/EFoodBLL/IntranetModels/Consecutive.cs
@@ -1,27 +1,44 @@
+using System.ComponentModel;
+
 namespace EFoodBLL.IntranetModels
 {
     public class Consecutive
     {
-        public int TypeConsecutive { get; set; }
+        [DisplayName("Tipo Consecutivo")]
+        public int TypeConsecutive { get; set; } 
+
+        [DisplayName("ID Consecutivo")]
         public int IdConsecutive { get; set; }
+        [DisplayName("Tiene Prefijo")]
         public bool HasPrefix { get; set; }
+   
+        [DisplayName("Prefijo")]
         public string? Prefix { get; set; }
     }
 
     public class ReturnConsecutive
     {
+        
         public int PkCode { get; set; }
+        [DisplayName("Tipo")]
         public int Type { get; set; }
+        [DisplayName("ID Consecutivo")]
         public int IdConsecutive { get; set; }
+        [DisplayName("Tiene Prefijo")]
         public bool HasPrefix { get; set; }
+        [DisplayName("Prefijo")]
         public string? Prefix { get; set; }
+        [DisplayName("Numero Consecutivo")]
         public int NumConsecutive { get; set; }
     }
 
     public class ConsecutiveList
     {
+        
         public int PkCode { get; set; }
+        [DisplayName("Tipo")]
         public string Type { get; set; }
+        [DisplayName("Consecutivo")]
         public int ConsecutiveId { get; set; }
     }
 }
