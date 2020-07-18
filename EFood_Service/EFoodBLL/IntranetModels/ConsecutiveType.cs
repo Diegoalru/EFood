eaaf98ded5afc0ada45aed6fc8078e691b1e6756
Diego Rubí Salas <diegoalru@gmail.com>
2020-07-17 20:29:07 -0600

    Cambio nombre de modelo.

--- EFood_Service/EFoodBLL/IntranetModels/ConsecutiveType.cs
+++ EFood_Service/EFoodBLL/IntranetModels/ConsecutiveType.cs
@@ -3,7 +3,7 @@ namespace EFoodBLL.IntranetModels
     /// <summary>
     /// Clase que puede ser utilizada para mostrar los tipos de consecutivos que existen.
     /// </summary>
-    public class ConsecutiveType
+    public class TypeConsecutive
     {
         public int PkCode { get; set; }
         public string Type { get; set; }
